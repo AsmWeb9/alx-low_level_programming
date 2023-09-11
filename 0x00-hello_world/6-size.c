94b05b7032f93683ed7f7bb1470f3bb402d66d72
AsmWeb9 <asmaa9nechadi@gmail.com>
2023-09-11 05:00:51 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - A program that prints the size of various computer types 
+ * main - A program that prints the size of various computer types
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
 	printf("Size of a char: %u byte(s)\n", sizeof(char));
