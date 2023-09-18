81440798988ed65ac34610026dd71098cff042bc
AsmWeb9 <asmaa9nechadi@gmail.com>
2023-09-18 11:04:29 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  _strlen - Takes a char pointer
+ *                        and calculat the legth of a string
+ * @s: It is a char pointer
+ * Return: the legth as an integer
+ */
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != "\0"; s++)
+		counter++;
+
+	return (counter);
+}
