42eaf06b1daa1c39a0d9101424cb0b82ee5eebd6
AsmWeb9 <asmaa9nechadi@gmail.com>
2023-09-22 07:55:17 -0700

    ./1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - a function that concatenates two strings.
+ * @dest: string parameter
+ * @src: string parameter
+ * @n: nomber of the letter  to concatenate
+ * Return: a strin
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+	while (dest[c])
+		c++;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	dest[c + i] = '\0';
+	return (dest);
+}
