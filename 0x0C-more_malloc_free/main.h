9ecb0259b01abf052a515bc39f4de933ac4fbb84
AsmWeb9 <asmaa9nechadi@gmail.com>
2023-10-09 04:14:25 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,9 +5,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
+
 #endif
