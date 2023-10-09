f69b726371895c6b85f6dda96a95565edbebd771
AsmWeb9 <asmaa9nechadi@gmail.com>
2023-10-09 06:27:02 -0700

    1-strconcat

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
+
 /**
  * *malloc_checked - allocates memory using malloc
  * and exit if failed
@@ -8,7 +9,7 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	int *m = malloc(b);
+	void *m = malloc(b);
 
 	if (m == 0)
 		exit(98);
