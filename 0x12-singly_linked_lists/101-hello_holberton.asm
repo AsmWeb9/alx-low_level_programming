76f59900c6f1f9d00777903c7b9458b3e7b672d1
AsmWeb9 <asmaa9nechadi@gmail.com>
2023-10-21 08:15:30 -0700

    last

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+section .data
+    hello db "Hello, Holberton", 10, 0  ; The string to be printed with a newline character
+
+section .text
+    global main
+
+    extern printf
+
+main:
+    push rbp
+    mov rdi, hello  ; Pass the address of the string to printf
+    call printf
+
+    ; Exit the program
+    mov rax, 60      ; syscall number for exit (60)
+    xor rdi, rdi     ; status = 0
+    syscall
+
+    pop rbp
+    ret
+
