16abaecadb7a80f6e46800d74b77c79189bb231d
AsmWeb9 <asmaa9nechadi@gmail.com>
2023-10-21 08:23:00 -0700

    add a file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,5 @@
 section .data
-    hello db "Hello, Holberton", 10, 0  ; The string to be printed with a newline character
+    hello db "Hello, Holberton", 0xa, 0  ; The string to be printed with a newline character
 
 section .text
     global main
