58777da45d806625df5412985a49f8366632f24d
AsmWeb9 <asmaa9nechadi@gmail.com>
2023-10-21 07:46:30 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - frees all nodes
+ * of a list
+ * @head: pointer to head node
+ * Return: void
+ */ 
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
